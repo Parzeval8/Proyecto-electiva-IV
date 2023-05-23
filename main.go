d851164acc4682e35648a1e144289aab1bfd4c63
Parzeval <cristian.ramirezp@outlook.com>
2023-05-23 01:43:54 -0500

    se agrega configuracion de los cors

--- main.go
+++ main.go
@@ -11,9 +11,26 @@ func main() {
 	config.Connect()
 	// se crea router para poder inicializar las rutas
 	router := gin.New()
+	router.Use(CORSMiddleware())
 
 	routes.PruebaRoute(router)
 	routes.EncuestaRoute(router)
 	routes.NewEncuestaRoute(router)
 	router.Run("localhost:8080")
+	// router.Run("localhost:9000")
+}
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
