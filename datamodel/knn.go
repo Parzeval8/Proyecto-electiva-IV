c4e01e66a98deff339363075907b0d7a5b85b2b6
Parzeval <cristian.ramirezp@outlook.com>
2023-05-31 09:19:49 -0500

    actualizacion knn

--- datamodel/knn.go
+++ datamodel/knn.go
@@ -32,9 +32,27 @@ func Knn(encuesta *models.NewEncuesta) {
 	vecinosCercanos = vecinosCercanos[:k]
 
 	// Calcular el valor de P33GraMoti basado en los vecinos cercanos
-	suma := 0
+	// suma := 0
+	// for _, distancia := range vecinosCercanos {
+	// 	suma += distancias[distancia]
+	// }
+	// encuesta.P33GraMoti = suma / k
+
+	// Calcular la moda de P33GraMoti basado en los vecinos cercanos
+	frecuencias := make(map[int]int)
 	for _, distancia := range vecinosCercanos {
-		suma += distancias[distancia]
+		p33GraMoti := distancias[distancia]
+		frecuencias[p33GraMoti]++
 	}
-	encuesta.P33GraMoti = suma / k
+
+	moda := -1
+	maxFrecuencia := -1
+	for valor, frecuencia := range frecuencias {
+		if frecuencia > maxFrecuencia {
+			moda = valor
+			maxFrecuencia = frecuencia
+		}
+	}
+
+	encuesta.P33GraMoti = moda
 }
